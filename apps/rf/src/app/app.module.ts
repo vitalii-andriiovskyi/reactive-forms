1a32746973eb03ec5972b3d4cbe1aecc39558349
Vitalii Andriiovskyi <domovik0712@gmail.com>
2022-07-05 19:17:54 +0300

    feat: add angular-material + styles

--- apps/rf/src/app/app.module.ts
+++ apps/rf/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule ,AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
